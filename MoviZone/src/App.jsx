5ab76a655f5856c03f4fb20ed1042e3c5f128dbf
RAJU YADAV <rajuyadav91391@gmail.com>
2025-06-09 14:41:25 +0530

    MovieZone Project Completed

---
+++ MoviZone/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Movies } from './components/Movies'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <Movies/>
+    </>
+  )
+}
+
+export default App
